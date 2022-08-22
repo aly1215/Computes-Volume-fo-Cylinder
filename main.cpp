d51777d91f81982f00f785116a37f5c36d25f5d6
Alyssa Mae Olaño <71934791+aly1215@users.noreply.github.com>
2022-08-22 15:55:50 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float pi = 3.14;
+  float rad,  h, vol;
+
+  cout<<"Enter the radius :"<<endl;
+  cin>>rad;
+
+  cout<<"height"<<endl;
+  cin>>h;
+
+  vol = pi * rad * rad * h;
+
+
+  cout<<"volume is n  " <<vol;
+    return 0;
+}
